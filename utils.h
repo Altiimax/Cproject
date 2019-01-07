86025cf51e4b70a60aafd225098229ae8cc84802
celiaHassaine <44391501+celiaHassaine@users.noreply.github.com>
2019-01-07 20:10:21 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,37 +1,43 @@
-	
 // Structures
 typedef struct{
-	int idV; // id de la voiture
-	int S1; //temps pris pour le secteur1
-	int S2;	//temps pris pour le secteur 2
-	int S3; //temps pris pour le secteur3 
-	int total; // total des 3 secteurs
-	bool dq;
-	int dqchance;
-	int fini; // si la voiture a fini la course
-	bool disq; //boolean, true: la voiture est disqualifié, false: la voiture ne l'est pas
+    int numero;
+    int idV; // id de la voiture
+    int S1; //temps pris pour le secteur1
+    int S2;    //temps pris pour le secteur 2
+    int S3; //temps pris pour le secteur3 
+    int total; // total des 3 secteurs
+    int meilleurTour; //meilleur tour de la voirute parmi tous ceux qui ont déjà été courrus
+    int totalTours;//temps total de la voiture depuis son départ à un moment donné (le maximum étant donné en fonction de quelle course on fait)
+    int nombreTours; //le nombre de tours déjà parcourus par la voirure 
+    bool dq;  //boolean, true: la voiture est disqualifié, false: la voiture ne l'est pas
+    int dqchance; //le pourcentage de chance de se faire disqualifier
+    bool fini; // si la voiture a fini la course
 } voiture;
 
 typedef struct { 
 	sem_t sem; 
 	int init_cars;
+	int fini;
 	voiture v[20];
 } shared_state_t;
 
 /**
 * Variable
 */
-//int S1=0, S2=0, S3=0, total=0;
-int tableau[3][20];
+
+int num[20]={44, 77, 5, 7, 3, 33, 11, 31, 18, 35, 27, 55, 10, 28, 8, 20, 2, 14, 9, 16};
+int S1=0, S2=0, S3=0, total=0, meilleur[20];
 shared_state_t* mem;
 int shmid;
+int pid;
 key_t key = 7816;
 
-/**
-*Fonction max
+/** 
+* fonction générant les nombres randoms
 */
-
-
+int rand_a_b(int a, int b){							
+	return rand()%(b-a) +a;																		
+}
 
 /**
 * tri du tableau
@@ -55,11 +61,112 @@ void ordonnerTableauVoitures(voiture tableau[], long tailleTableau) {
     tableau[tailleTableau-1]=rempl1;
 }
 
-/** 
-* fonction générant les nombres randoms
+/**
+* Affichage lorsque la voiture a fini son tour
 */
-int rand_a_b(int a, int b){							
-	return rand()%(b-a) +a;																		
+
+void affichage(voiture tableau[], int j, int sec){
+	tableau[j].dqchance = rand_a_b(0,100000);
+	if( sec == 0){
+		total = S1+S2+S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d \n", tableau[j].numero, tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d \n",tableau[j].numero,S1,S2,S3,total, tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	} else if(sec == 1){
+		total = tableau[j].S1+S2+S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d \n", tableau[j].numero,tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d \n",tableau[j].numero,tableau[j].S1,S2,S3,total,tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	} else {
+		total = tableau[j].S1+tableau[j].S2+S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d\n", tableau[j].numero,tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d\n",tableau[j].numero,tableau[j].S1,tableau[j].S2,S3,total,tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	}
+}
+
+/**
+* Affichage lorsque la voiture a fini son tour
+*/
+
+void affichageFini(voiture tableau[], int j, int sec){
+	tableau[j].dqchance = rand_a_b(0,100000);
+	if( sec == 0){
+		total = tableau[j].S1+S2+S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d\n", tableau[j].numero,tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d\n",tableau[j].numero,tableau[j].S1,S2,S3,total,tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	} else if(sec == 1){
+		total = tableau[j].S1+tableau[j].S2+S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d\n", tableau[j].numero,tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d\n",tableau[j].numero,tableau[j].S1,tableau[j].S2,S3,total,tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	} else {
+		total = tableau[j].S1+tableau[j].S2+tableau[j].S3;
+		if(tableau[j].dq==true){
+			fprintf(stderr, "\n  %d \tDQ \tDQ \tDQ \tDQ \t%d\n", tableau[j].numero,tableau[j].meilleurTour);
+		} else {
+			fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \t%d\n",tableau[j].numero,tableau[j].S1,tableau[j].S2,tableau[j].S3,total,tableau[j].meilleurTour);
+			if(tableau[j].dqchance<=5){
+				tableau[j].dq = true;
+			}
+			if(tableau[j].dqchance>5){
+				tableau[j].dq = false;
+			}
+		}
+	}
+}
+
+/**
+* remettre a 0 les variables utiles pour l affichage
+*/
+
+void reset(voiture tableau[]){
+	S1=0, S2=0, S3=0, total=0;
+	mem->fini = 0;
+	for (int j=0; j<20; j++){
+		tableau[j].fini =false;
+	}
 }
 
 /**
@@ -99,10 +206,143 @@ void detShm(int shmid, shared_state_t* mem){
 * Sémaphore partie
 */
 
-/*void semInit(shared_state_t* stru){
-	if( (sem_init(stru->sem, 1, 1)) < 0 ) {	
-		perror("erreur semget");
-		exit(1);
+void genUnTour(voiture tabVoitures[]){
+	for(int i=0;i<20;i++){
+		pid=fork(); //création du fork
+		if(pid==-1){
+			printf("impossible de créer un fils\n");
+			exit(1);
+		}
+		if(pid==0){ 
+			voiture voit;
+			voit.idV = getpid();
+			srand(time(0)^(getpid()));
+			//Ecriture dans la variable à transmettre "voit"
+			voit.S1=rand_a_b(20,40);
+			voit.S2=rand_a_b(20,40);
+			voit.S3=rand_a_b(20,40);
+			voit.dqchance = 0;
+			voit.dq = false;
+			voit.fini = 0;
+			memcpy(&(mem->v[i]), &voit, sizeof(voit));
+			if( (sem_wait(&(mem->sem))) < 0 ) {	
+				perror("erreur semget");
+				exit(1);
+			}
+			(mem->init_cars)++;
+			if( (sem_post(&(mem->sem))) < 0 ) {	
+				perror("erreur semget");
+				exit(1);
+			}
+			printf("Numéro %d: %d, %d, %d, %d, initcars = %d\n",i ,mem->v[i].S1, mem->v[i].S2, mem->v[i].S3, mem->v[i].idV, mem->init_cars);
+			detShm(shmid, mem);
+			exit(1);
+		}
 	}
-}*/
+	while(1){
+		if( (sem_wait(&(mem->sem))) < 0 ) {	
+			perror("erreur semget");
+			exit(1);
+		}
+		if(mem->init_cars == 19){
+			sem_post(&(mem->sem));
+			break;
+		}else{
+			sem_post(&(mem->sem));
+			sleep(1);
+		}
+	}
+	for (int i=0;i<20;i++){
+		tabVoitures[i] = mem->v[i];
+		tabVoitures[i].numero = num[i];
+		for(int i=0; i<20; i++){
+			tabVoitures[i].meilleurTour = meilleur[i];
+		}
+		tabVoitures[i].total = tabVoitures[i].S1+tabVoitures[i].S2+tabVoitures[i].S3;
+		fprintf(stderr, "\n  %d \t%d \t%d \t%d \t%d \n",tabVoitures[i].idV,tabVoitures[i].S1,tabVoitures[i].S2,tabVoitures[i].S3,tabVoitures[i].total);
+	}
+}
+
+/**
+* Affichage D'un tour complet!
+*/
+
+void afficheUnTour(voiture tabVoitures []){
+	reset(tabVoitures);
+	while(mem->fini != 20){
+		//sem_wait(&(mem->sem));
+		system("clear");
+		fprintf(stderr, "Numéro \tS1 \tS2 \tS3 \tTotal \tMeilleur tour");
+		++S1;
+		for(int j=0; j<20; j++){
+			sem_wait(&(mem->sem));
+			if (S1 == tabVoitures[j].S1){
+				tabVoitures[j].fini = mem->fini ;
+				affichageFini(tabVoitures, j, 0);
+				++(mem->fini);
+			} else if (tabVoitures[j].fini != 0){ 
+				affichageFini(tabVoitures, j, 0);
+			} else {
+				affichage(tabVoitures, j, 0);
+			}
+			sem_post(&(mem->sem));
+		}
+		usleep(0);
+	}
+	reset(tabVoitures);
+	while(mem->fini != 20){
+		system("clear");
+		fprintf(stderr, "Numéro \tS1 \tS2 \tS3 \tTotal \tMeilleur tour");
+		++S2;
+		for(int j=0; j<20; j++){
+			sem_wait(&(mem->sem));
+			if (S2 == tabVoitures[j].S2){
+				tabVoitures[j].fini = true ;
+				affichageFini(tabVoitures, j, 1);
+				++(mem->fini);
+			} else if (tabVoitures[j].fini){ 
+				affichageFini(tabVoitures, j, 1);
+			} else {
+				affichage(tabVoitures, j, 1);
+			}
+			sem_post(&(mem->sem));
+		}
+		usleep(0);
+	}
+	reset(tabVoitures);
+	while(mem->fini != 20){
+		system("clear");
+		fprintf(stderr, "Numéro \tS1 \tS2 \tS3 \tTotal \tMeilleur tour");
+		++S3;
+		for(int j=0; j<20; j++){
+			sem_wait(&(mem->sem));
+			if (S3 == tabVoitures[j].S3){
+				tabVoitures[j].fini = mem->fini ;
+				affichageFini(tabVoitures, j, 2);
+				++(mem->fini);
+			} else if (tabVoitures[j].fini){ 
+				affichageFini(tabVoitures, j, 2);
+			} else {
+				affichage(tabVoitures, j, 2);
+			}
+			sem_post(&(mem->sem));
+		}
+		usleep(0);
+	}
+	for(int i=0;i<20;i++){
+		fprintf(stderr, "%d\n",tabVoitures[i].dq);
+		if(meilleur[i] < tabVoitures[i].total && tabVoitures[i].dq==0){
+			meilleur[i] = tabVoitures[i].total;
+		}
+	}
+	for(int i=0;i<20;i++){
+		tabVoitures[i].meilleurTour = meilleur[i];
+	}
+	system("clear");
+	fprintf(stderr, "Numéro \tS1 \tS2 \tS3 \tTotal \tMeilleur tour");
+	for(int j = 0; j<20;j++){
+		affichageFini(tabVoitures, j, 2);
+	}
+	detShm(shmid, mem);
+}
 
